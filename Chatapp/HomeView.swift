10bddb5f13edeb335877e40a20546801c2a96ba7
Shige <83544518+ST0927@users.noreply.github.com>
2023-11-01 17:19:30 +0900

    11/01
    1.プレヴューは見れてもビルドができないエラーを修正。原因はContentView内のChat()呼び出しにenvironmentObjectがなかったこと
    2.Loggerの中にコンテンツを捩じ込むんじゃなくてLoggerをstructとして作って呼び出す形に変更
    
    プレヴュー、ビルド動作確認済み

--- Chatapp/HomeView.swift
+++ Chatapp/HomeView.swift
@@ -22,83 +22,92 @@ struct Chat: View {
         ChatMessage(role: .system, content: "あなたは、ユーザーの質問や会話に回答するロボットです。"),
         ChatMessage(role: .system, content:"こんにちは。何かお困りのことがあればおっしゃってください。")
     ]
+    //どっかでpublishにする必要ありそう
+     @State var tapNum:Int = 0
+     @EnvironmentObject var timerController: TimerCount
     
     // チャット画面のビューレイアウト
     var body: some View {
-        VStack {
-            // スクロール可能なメッセージリストの表示
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach(chat.indices, id: \.self) { index in
-                        // 最初のメッセージ以外を表示
-                        if index > 1 {
-                            MessageView(message: chat[index])
+        ZStack {
+            Logger()
+                .environmentObject(TimerCount())
+            //ここから
+            VStack {
+                // スクロール可能なメッセージリストの表示
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        ForEach(chat.indices, id: \.self) { index in
+                            // 最初のメッセージ以外を表示
+                            if index > 1 {
+                                MessageView(message: chat[index])
+                            }
                         }
                     }
                 }
-            }
-            .padding(.top)
-            // 画面をタップしたときにキーボードを閉じる
-            .onTapGesture {
-                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-            }
-            
-            // テキスト入力フィールドと送信ボタンの表示
-            HStack {
-                // テキスト入力フィールド
-                TextField("メッセージを入力", text: $text)
-                    .disabled(isCompleting) // チャットが完了するまで入力を無効化
-                    .font(.system(size: 15)) // フォントサイズを調整
-                    .padding(8)
-                    .padding(.horizontal, 10)
-                    .background(Color.white) // 入力フィールドの背景色を白に設定
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray.opacity(0.5), lineWidth: 1.5)
-
-                    )
+                .padding(.top)
+                // 画面をタップしたときにキーボードを閉じる
+                .onTapGesture {
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }
                 
-                // 送信ボタン
-                Button(action: {
-                    isCompleting = true
-                    // ユーザーのメッセージをチャットに追加
-                    chat.append(ChatMessage(role: .user, content: text))
-                    text = "" // テキストフィールドをクリア
+                // テキスト入力フィールドと送信ボタンの表示
+                HStack {
+                    // テキスト入力フィールド
+                    TextField("メッセージを入力", text: $text)
+                        .disabled(isCompleting) // チャットが完了するまで入力を無効化
+                        .font(.system(size: 15)) // フォントサイズを調整
+                        .padding(8)
+                        .padding(.horizontal, 10)
+                        .background(Color.white) // 入力フィールドの背景色を白に設定
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray.opacity(0.5), lineWidth: 1.5)
+
+                        )
                     
-                    Task {
-                        do {
-                            // OpenAIの設定
-                            let config = Configuration(
-                                organizationId: "org-eWKJ3cXychFNfqig8QpaymWY",
-                                apiKey: "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"
-                            )
-                            let openAI = OpenAI(config)
-                            let chatParameters = ChatParameters(model: "gpt-3.5-turbo", messages: chat)
-                            
-                            // チャットの生成
-                            let chatCompletion = try await openAI.generateChatCompletion(
-                                parameters: chatParameters
-                            )
-                            
-                            isCompleting = false
-                            // AIのレスポンスをチャットに追加
-                            chat.append(ChatMessage(role: .assistant, content: chatCompletion.choices[0].message.content))
-                        } catch {
-                            print("ERROR DETAILS - \(error)")
+                    // 送信ボタン
+                    Button(action: {
+                        isCompleting = true
+                        // ユーザーのメッセージをチャットに追加
+                        chat.append(ChatMessage(role: .user, content: text))
+                        text = "" // テキストフィールドをクリア
+                        
+                        Task {
+                            do {
+                                // OpenAIの設定
+                                let config = Configuration(
+                                    organizationId: "org-eWKJ3cXychFNfqig8QpaymWY",
+                                    apiKey: "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"
+                                )
+                                let openAI = OpenAI(config)
+                                let chatParameters = ChatParameters(model: "gpt-3.5-turbo", messages: chat)
+                                
+                                // チャットの生成
+                                let chatCompletion = try await openAI.generateChatCompletion(
+                                    parameters: chatParameters
+                                )
+                                
+                                isCompleting = false
+                                // AIのレスポンスをチャットに追加
+                                chat.append(ChatMessage(role: .assistant, content: chatCompletion.choices[0].message.content))
+                            } catch {
+                                print("ERROR DETAILS - \(error)")
+                            }
                         }
+                    }) {
+                        // 送信ボタンのデザイン
+                        Image(systemName: "arrow.up.circle.fill")
+                            .font(.system(size: 30))
+                            .foregroundColor(self.text == "" ? Color(#colorLiteral(red: 0.75, green: 0.95, blue: 0.8, alpha: 1)) : Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)))
                     }
-                }) {
-                    // 送信ボタンのデザイン
-                    Image(systemName: "arrow.up.circle.fill")
-                        .font(.system(size: 30))
-                        .foregroundColor(self.text == "" ? Color(#colorLiteral(red: 0.75, green: 0.95, blue: 0.8, alpha: 1)) : Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)))
+                    // テキストが空またはチャットが完了していない場合はボタンを無効化
+                    .disabled(self.text == "" || isCompleting)
                 }
-                // テキストが空またはチャットが完了していない場合はボタンを無効化
-                .disabled(self.text == "" || isCompleting)
+                .padding(.horizontal)
+                .padding(.bottom, 8) // 下部のパディングを調整
             }
-            .padding(.horizontal)
-            .padding(.bottom, 8) // 下部のパディングを調整
         }
+            //ここまで
     }
 }
 
@@ -155,10 +164,30 @@ struct AvatarView: View {
     }
 }
 
-// プレビュー
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        Chat()
+struct Logger : View {
+    @State var tapNum:Int = 0
+    @EnvironmentObject var timerController: TimerCount
+    
+    var body: some View {
+        //透明なビューを設置してタップ回数のカウント
+            Color.clear
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    tapNum += 1
+                    //リセットない？TimerCoutの方でfunc作ればできそう
+                    timerController.count = 0
+                    timerController.start(0.1)
+                }
+        //動作確認用
+        HStack {
+            VStack {
+                Text("タップ回数：\(tapNum)")
+                Text("タップ間隔：\(timerController.count)")
+            }
+        }
     }
 }
 
+#Preview {
+    Chat()
+}
