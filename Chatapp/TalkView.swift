e28516a22f907c3bffd2eecde288bbef6f162fd3
Shige <83544518+ST0927@users.noreply.github.com>
2023-12-07 22:26:38 +0900

    Update TalkView.swift
    UI変更
    チャットボックスの上端揃え
    画像の拡大
    ボタンの非表示（画像のボタン埋め込みはまだ）
    背景色の変更

--- Chatapp/TalkView.swift
+++ Chatapp/TalkView.swift
@@ -13,132 +13,166 @@ struct Talk: View {
     @State var message = ""
     @State var history: [Message] = []
     @State var button =  ""
-    @State var answer: [Answer] = []
     @EnvironmentObject var Q: QuestionList
     @State private var isButtonDisabled: Bool = false
-
+    @State private var bot: Bool = false
+    @State var start:Bool = false
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            ScrollView {
-                //ForEachでanswer配列追加できそう
-                ForEach(history.indices, id: \.self) { index in
-                    let Num = index+1 //indexがIntじゃないから数字を足す
-                    HStack {
-                        //ユーザー
-                        if isButtonDisabled == true {
-                            Spacer()
-                            Text("\(Num): \(answer[index].text)")
-                                .font(.system(size: 14))
-                                .padding(10)
-                                .background(Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)))
-                                .cornerRadius(10)
-                        }
-                        if Num % 2 == 1 {
-                            Spacer()
-                            Text("\(Num): \(history[index].text)")
-                                .font(.system(size: 14))
-                                .padding(10)
-                                .background(Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)))
-                                .cornerRadius(10)
-                        } else {
-                            //チャットボット
+        ZStack {
+            Color(red:0.4549,green:0.5804,blue:0.7529,opacity:1.0)
+                .ignoresSafeArea(edges: [.bottom])
+            VStack(alignment: .leading) {
+                ScrollView {
+                    if start == false {
+                        HStack(alignment: .top) {
                             AvatarView(imageName: "avatar")
                                 .padding(.trailing, 8)
                             VStack(spacing: 0) {
-                                HStack(spacing: 0) {
-                                    if Num/2 <= (Q.ImageName.count/2) {
-                                        Image(Q.ImageName[Num - 2])
-                                            .resizable()
-                                            .frame(width: 60, height: 60)
-                                            .border(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)), width: 1)
-                                        Image(Q.ImageName[Num - 1])
-                                            .resizable()
-                                            .frame(width: 60, height: 60)
-                                            .border(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)), width: 1)
-                                    }
-                                }
-                                Text(" A or B ")
-                                    .frame(width: 100)
+                                Text("アンケートを始めますか？")
+                                    .frame(width: 200)
                                     .font(.system(size: 14))
                                     .padding(10)
                                     .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
                                 Button(action: {
-                                    isButtonDisabled = true
-                                    self.button = "A"
-                                    if Num % 2 == 0 {
-                                        let db = Firestore.firestore()
-                                        db.collection("answers").addDocument(data: ["Q": self.button]) { err in
-                                            if let e = err {
-                                                print(e)
-                                            } else {
-                                                print("sent")
-                                            }
+                                    start = true
+                                    self.button = "アンケートを始める"
+                                    let db = Firestore.firestore()
+                                    db.collection("messages").addDocument(data: ["text": self.button]) { err in
+                                        if let e = err {
+                                            print(e)
+                                        } else {
+                                            print("sent")
                                         }
-                                        self.button = ""
                                     }
-                                    isButtonDisabled = false
                                 })
                                 {
-                                    Text("A")
-                                        .frame(width: 100)
+                                    Text("はい")
+                                        .frame(width: 200)
                                         .padding(10)
                                         .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
                                 }.disabled(isButtonDisabled)
-                                Button(action: {
-
-                                }){
-                                    Text("B")
-                                        .frame(width: 100)
-                                        .padding(10)
-                                        .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
-                                }
                             }
                             Spacer()
                         }
-                    }.padding(.horizontal)
-                }.padding(.vertical, 5)
-            }
-            HStack {
-                TextField("メッセージ", text: $message).padding()
-                Button(action: {
-                    let db = Firestore.firestore()
-                    db.collection("messages").addDocument(data: ["text":self.message]) { err in
-                        if let e = err {
-                            print(e)
-                        } else {
-                            print("sent")
-                        }
+                    } else {
+                        ForEach(history.indices, id: \.self) { index in
+                            let Num = index+1 //indexがIntじゃないから数字を足す
+                            
+                            HStack {
+                                Spacer()
+                                Text("\(Num): \(history[index].text)")
+                                    .font(.system(size: 14))
+                                    .padding(10)
+                                    .background(Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)))
+                                    .cornerRadius(10)
+                            }.padding(.horizontal)
+                            
+                            HStack(alignment: .top) {
+                                
+                                AvatarView(imageName: "avatar")
+                                    .padding(.trailing, 8)
+
+                                VStack(spacing: 0) {
+                                    
+                                    Text(" A or B ")
+                                        .frame(width: 280)
+                                        .font(.system(size: 14))
+                                        .padding(10)
+                                        .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
+                                    HStack(spacing: 0) {
+                                        if Num/2 <= (Q.ImageName.count/4) {
+                                            Image(Q.ImageName[Num*2 - 2])
+                                                .resizable()
+                                                .frame(width: 150, height: 150)
+                                                .border(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)), width: 1)
+                                            Image(Q.ImageName[Num*2 - 1])
+                                                .resizable()
+                                                .frame(width: 150, height: 150)
+                                                .border(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)), width: 1)
+                                        }
+                                    }
+//                                    HStack(spacing: 0) {
+//                                        Button(action: {
+//                                            self.button = "A" //ボタンテキストの中身引っ張りたい
+//                                            let db = Firestore.firestore()
+//                                            db.collection("messages").addDocument(data: ["text": self.button]) { err in
+//                                                if let e = err {
+//                                                    print(e)
+//                                                } else {
+//                                                    print("sent")
+//                                                }
+//                                            }
+//                                        })
+//                                        {
+//                                            Text("A")
+//                                                .frame(width: 150)
+//                                                .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
+//                                        }.disabled(isButtonDisabled)
+//                                        Button(action: {
+//                                            self.button = "B"
+//                                            let db = Firestore.firestore()
+//                                            db.collection("messages").addDocument(data: ["text": self.button]) { err in
+//                                                if let e = err {
+//                                                    print(e)
+//                                                } else {
+//                                                    print("sent")
+//                                                }
+//                                            }
+//                                        }){
+//                                            Text("B")
+//                                                .frame(width: 150)
+//                                                .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
+//                                        }
+//                                    }
+                                    .disabled(isButtonDisabled)
+                                }
+                                Spacer()
+                            }.padding(.horizontal)
+                            
+                        }.padding(.vertical, 5)
                     }
-                    self.message = ""
-                }) {
-                    Image(systemName:"paperplane.fill").padding()
+                    
+                    
+                }
+                    
+            }.keyboardObserving().onAppear  {
+                let db = Firestore.firestore()
+                db.collection("messages").addSnapshotListener {
+                    (snapshot, err) in
+                    if err != nil {
+                        print("error")
+                    } else {
+                        snapshot?.documentChanges.forEach({(diff) in
+                            if diff.type == .added {
+                                let m = Message(data: diff.document.data())
+                                self.history.append(m)
+                            }
+                        })
+                    }
                 }
             }
-        }.keyboardObserving().onAppear  {
-            let db = Firestore.firestore()
-            db.collection("messages").addSnapshotListener {
-                (snapshot, err) in
-                if err != nil {
-                    print("error")
-                } else {
-                    snapshot?.documentChanges.forEach({(diff) in
-                        if diff.type == .added {
-                            let m = Message(data: diff.document.data())
-                            self.history.append(m)
+            VStack {
+                Spacer()
+                HStack(spacing:0) {
+                    TextField("メッセージ", text: $message)
+                        .frame(height: 55)
+                        .background(Color.white)
+                    Button(action: {
+                        let db = Firestore.firestore()
+                        db.collection("messages").addDocument(data: ["text":self.message]) { err in
+                            if let e = err {
+                                print(e)
+                            } else {
+                                print("sent")
+                            }
                         }
-                    })
-                }
-            }
-            db.collection("answers").addSnapshotListener {
-                (snapshot, err) in
-                if err != nil {
-                    print("error")
-                } else {
-                    snapshot?.documentChanges.forEach({(diff) in
-                        if diff.type == .added {
-                            let a = Answer(data: diff.document.data())
-                            self.answer.append(a)                        }
-                    })
+                        self.message = ""
+                    }) {
+                        Image(systemName:"paperplane.fill")
+                            .frame(width: 55,height: 55)
+                            .background(Color.white)
+                    }
                 }
             }
         }
@@ -154,16 +188,9 @@ struct Message : Identifiable {
     }
 }
 
-struct Answer : Identifiable {
-    var id = UUID()
-    var text: String
-    
-    init(data: [String: Any]) {
-        self.text = data["Q"] as! String
-    }
-}
-
 #Preview {
     Talk()
         .environmentObject(QuestionList())
 }
+
+
