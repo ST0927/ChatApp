ac1d97ef70be95e902aac0a2c8c1049d636201e3
Shige <83544518+ST0927@users.noreply.github.com>
2024-01-12 16:01:24 +0900

    Update TalkView.swift
    スクロール長さ、時間、速度取得の実装

--- Chatapp/TalkView.swift
+++ Chatapp/TalkView.swift
@@ -53,14 +53,25 @@ struct Talk: View {
     @State var message = ""
     @State var history: [Message] = []
     @EnvironmentObject var Q: QuestionList
-    @EnvironmentObject var timerController: TimerCount
     @State var isButtonDisabled: Bool = false
     //アンケートを開始するかを決める変数
     @State var start:Bool = false
-    
-    @State  private var offsetY: CGFloat = 0
-    @State  private var initOffsetY: CGFloat = 0
-    
+    //
+    @State var offsetY: CGFloat = 0
+    @State var initOffsetY: CGFloat = 0
+    @State var pre: CGFloat = 0
+    @State var current: CGFloat = 0
+    @State var scroll: Bool = false
+    @State var Time: AnyCancellable?
+    @State var startposition: CGFloat = 0
+    @State var endposition: CGFloat = 0
+    @State var ScrollTime: AnyCancellable?
+    @State var ScrollTimeCount:Double = 0
+    @State var ScrollingTime:Double = 0
+    @State var ScrollSpeed:Double = 0
+    @State var unScrollTime: AnyCancellable?
+    @State var unScrollTimeCount:Double = 0
+    @State var UnScrollTimeCount:Double = 0
     
     var body: some View {
         ZStack {
@@ -80,6 +91,14 @@ struct Talk: View {
                                     .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
                                 Button(action: {
                                     start = true
+                                    let db = Firestore.firestore()
+                                    db.collection("messages").addDocument(data: ["text": "始める"]) { err in
+                                        if let e = err {
+                                            print(e)
+                                        } else {
+                                            print("sent")
+                                        }
+                                    }
                                 })
                                 {
                                     Text("はい")
@@ -113,12 +132,12 @@ struct Talk: View {
                                         .background(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
                                     HStack(spacing: 0) {
                                         if Num/2 <= (Q.ImageName.count/4) {
-                                            Image(""/*Q.ImageName[Num*2 - 2]*/)
+                                            Image(Q.ImageName[Num*2 - 2])
                                                 .resizable()
                                                 .frame(width: 150, height: 150)
                                                 .border(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)), width: 1)
                                                 .background(Color.white)
-                                            Image(""/*Q.ImageName[Num*2 - 1]*/)
+                                            Image(Q.ImageName[Num*2 - 1])
                                                 .resizable()
                                                 .frame(width: 150, height: 150)
                                                 .border(Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)), width: 1)
@@ -144,11 +163,65 @@ struct Talk: View {
                 }.padding(.bottom, 55)
                     .onPreferenceChange(ScrollOffsetYPreferenceKey.self) { value in
                         offsetY = value[0]
+                        if scroll == false {
+                            print("start")
+                            startposition = offsetY - initOffsetY
+                            UnScrollTimeCount = unScrollTimeCount
+                            
+                            if let _timer = ScrollTime{
+                                _timer.cancel()
+                            }
+                            ScrollTime = Timer.publish(every: 0.1, on: .main, in: .common)
+                                .autoconnect()
+                                .receive(on: DispatchQueue.main)
+                                .sink { _ in
+                                    ScrollTimeCount += 0.1
+                                }
+                        }
+                        scroll = true
+                        current = offsetY - initOffsetY
                         print(offsetY - initOffsetY)
-                }
+                        if let _timer = Time{
+                            _timer.cancel()
+                        }
+                        Time = Timer.publish(every: 0.1, on: .main, in: .common)
+                            .autoconnect()
+                            .receive(on: DispatchQueue.main)
+                            .sink { _ in
+                                if scroll == true {
+                                    if pre == current {
+                                        print("end")
+                                        endposition = offsetY - initOffsetY
+                                        ScrollingTime = ScrollTimeCount
+                                        ScrollSpeed = (endposition - startposition)/ScrollingTime
+                                        ScrollTimeCount = 0
+                                        scroll = false
+                                    } else {
+                                        print("スクロール中")
+                                    }
+                                }
+                                else if pre == current {
+                                    unScrollTimeCount = 0
+                                    if let _timer = unScrollTime{
+                                        _timer.cancel()
+                                    }
+                                    unScrollTime = Timer.publish(every: 0.1, on: .main, in: .common)
+                                        .autoconnect()
+                                        .receive(on: DispatchQueue.main)
+                                        .sink { _ in
+                                            unScrollTimeCount += 0.1
+                                        }
+                                }
+                            }
+                        pre = offsetY - initOffsetY
+                    }
+                
                 .onTapGesture {
                     UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                 }
+                
+                
+                
             }.keyboardObserving().onAppear  {
                 let db = Firestore.firestore()
                 db.collection("messages").addSnapshotListener {
@@ -190,7 +263,7 @@ struct Talk: View {
                 }
             }
             if start == true {
-                Logger(offsetY: $offsetY, initOffsetY: $initOffsetY)
+                Logger(offsetY: $offsetY, initOffsetY: $initOffsetY, pre: $pre, current: $current, scroll: $scroll, startposition: $startposition, endposition: $endposition, ScrollingTime: $ScrollingTime, ScrollSpeed: $ScrollSpeed, UnScrollTimeCount: $UnScrollTimeCount)
                     .environmentObject(TimerCount())
             }
         }
@@ -211,8 +284,18 @@ struct Logger : View {
     @State var RightChoice:Int = 0
     @State var TimeCount:Double = 0
     @State var time: AnyCancellable?
+    
     @Binding var offsetY:CGFloat
     @Binding var initOffsetY:CGFloat
+    @Binding var pre: CGFloat
+    @Binding var current: CGFloat
+    @Binding var scroll: Bool
+    @Binding var startposition: CGFloat
+    @Binding var endposition: CGFloat
+    @Binding var ScrollingTime:Double
+    @Binding var ScrollSpeed:Double
+    @Binding var UnScrollTimeCount:Double
+   
 
     var body: some View {
         //透明なビューを設置してタップ回数のカウント
@@ -220,7 +303,6 @@ struct Logger : View {
             .contentShape(Rectangle())
             .onTapGesture {
                 tapNum += 1
-                
                 TimeCount = 0
                 if let _timer = time{
                     _timer.cancel()
@@ -231,7 +313,6 @@ struct Logger : View {
                     .sink { _ in
                         TimeCount += 0.1
                     }
-
             }
         //動作確認用
         HStack {
@@ -240,7 +321,11 @@ struct Logger : View {
                 Text("タップ間隔：\(TimeCount)")
                 Text("左を選んだ回数：\(LeftChoice)")
                 Text("右を選んだ回数：\(RightChoice)")
-                Text("スクロール位置：\(offsetY - initOffsetY)")
+                Text("画面位置：\(offsetY - initOffsetY)")
+                Text("スクロール長さ：\(endposition - startposition)")
+                Text("スクロール時間：\(ScrollingTime)")
+                Text("スクロール速度：\(abs(ScrollSpeed))")
+//                Text("スクロール間隔：\(UnScrollTimeCount)")
                 
             }
         }
@@ -339,4 +424,5 @@ struct Message : Identifiable {
     Talk()
         .environmentObject(QuestionList())
 }
+
 
